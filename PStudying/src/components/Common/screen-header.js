57b87f5fafc0466685a40a5ebee254d29fb4279d
1712397-KD <1712397@student.hcmus.edu.vn>
2020-11-10 19:30:41 +0700

    finish download screen

--- PStudying/src/components/Common/screen-header.js
+++ PStudying/src/components/Common/screen-header.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 import css from "../../globals/style";
 const ScreenHeader = ({screenTitle}) => {
     return (
-        <View>
+        <View style={{paddingVertical: 20}}>
             <Text style={css.screenTitle}>{screenTitle}</Text>
         </View>
     );
