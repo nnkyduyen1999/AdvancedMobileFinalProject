bf1ea0c0f8ae18df040eded25d8392e96965b265
1712397-KD <1712397@student.hcmus.edu.vn>
2020-11-26 00:57:15 +0700

    remove status state in login

--- PStudying/src/components/Authentication/Login/login.js
+++ PStudying/src/components/Authentication/Login/login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { View, TouchableOpacity, Alert, Text } from "react-native";
 import { Button } from "react-native-elements";
 import InputCustom from "./InputText/input-text";
@@ -7,87 +7,80 @@ import css from "../../../globals/style";
 import theme from "../../../globals/theme";
 import constant from "../../../globals/constant";
 import authenticationService from "../../../core/services/authentication-services";
-import {AuthenticationContext} from "../../../providers/authentication-provider"
+import { AuthenticationContext } from "../../../providers/authentication-provider";
 
 const Login = ({ navigation }) => {
   const [username, setUsername] = useState(``);
   const [password, setPassword] = useState(``);
-  const [status, setStatus] = useState(null);
+  const authenticationContext = useContext(AuthenticationContext);
 
-  const renderStatus = (status) => {
-    if (!status) {
+  const { authentication } = authenticationContext;
+
+  const renderStatus = (authentication) => {
+    if (!authentication) {
       return <></>;
-    } else if (status.statusCode === 200) {
+    } else if (authentication.statusCode === 200) {
       return <Text style={{ color: "red" }}>Successfully Login</Text>;
     } else {
-      return <Text style={{ color: "red" }}>{status.errString}</Text>;
+      return <Text style={{ color: "red" }}>{authentication.errString}</Text>;
     }
   };
 
   useEffect(() => {
-    if (status && status.statusCode === 200) {
+    if (authentication && authentication.statusCode === 200) {
       navigation.navigate(constant.navigationNames.Home);
     }
-  }, [status]);
+  }, [authentication]);
 
   return (
-    <AuthenticationContext.Consumer>
-      {({setAuthentication}) => {
-        return (
-          <View
-            style={[
-              css.screenContentNoPaddingTop,
-              { justifyContent: "center" },
-            ]}
-          >
-            <View style={{ height: 400, justifyContent: "space-around" }}>
-              {renderStatus(status)}
-              <InputCustom
-                label="Username"
-                placeholder="Email or username"
-                iconName="envelope-o"
-                textChange={(text) => setUsername(text)}
-              />
-              <InputCustom
-                label="Password"
-                placeholder="Password"
-                iconName="unlock-alt"
-                rightIcon="eye-slash"
-                textChange={(text) => setPassword(text)}
-              />
-              <Button
-                title="Sign in"
-                buttonStyle={[{ backgroundColor: theme.BASIC_BLUE }]}
-                titleStyle={css.authenBtnTitle}
-                onPress={() => {
-                  setStatus(authenticationService.login(username, password));
-                  setAuthentication(authenticationService.login(username, password));
-                }}
-              />
+    <View style={[css.screenContentNoPaddingTop, { justifyContent: "center" }]}>
+      <View style={{ height: 400, justifyContent: "space-around" }}>
+        {renderStatus(authentication)}
+        <InputCustom
+          label="Username"
+          placeholder="Email or username"
+          iconName="envelope-o"
+          textChange={(text) => setUsername(text)}
+        />
+        <InputCustom
+          label="Password"
+          placeholder="Password"
+          iconName="unlock-alt"
+          rightIcon="eye-slash"
+          textChange={(text) => setPassword(text)}
+        />
+        <Button
+          title="Sign in"
+          buttonStyle={[{ backgroundColor: theme.BASIC_BLUE }]}
+          titleStyle={css.authenBtnTitle}
+          onPress={() => {
+            //setStatus(authenticationService.login(username, password));
+            authenticationContext.setAuthentication(
+              authenticationService.login(username, password)
+            );
+          }}
+        />
 
-              <TouchableOpacity
-                onPress={() =>
-                  navigation.navigate(constant.navigationNames.ForgotPassword)
-                }
-              >
-                <TextButton txt="Forget password?" />
-              </TouchableOpacity>
-              <Button
-                title="Use Single Signin-On (SSO)"
-                type="outline"
-                titleStyle={css.authenBtnTitleOutline}
-              />
-              <Button
-                title="Cancel"
-                type="outline"
-                titleStyle={css.authenBtnTitleOutline}
-                onPress={() => navigation.goBack()}
-              />
-            </View>
-          </View>
-        );
-      }}
-    </AuthenticationContext.Consumer>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate(constant.navigationNames.ForgotPassword)
+          }
+        >
+          <TextButton txt="Forget password?" />
+        </TouchableOpacity>
+        <Button
+          title="Use Single Signin-On (SSO)"
+          type="outline"
+          titleStyle={css.authenBtnTitleOutline}
+        />
+        <Button
+          title="Cancel"
+          type="outline"
+          titleStyle={css.authenBtnTitleOutline}
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+    </View>
   );
 };
 
