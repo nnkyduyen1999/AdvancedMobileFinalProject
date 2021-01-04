7afb9660544a50156ab9411f7c4e6c41a6213fc1
1712397-KD <1712397@student.hcmus.edu.vn>
2021-01-04 17:36:04 +0700

    play youtube video

--- PStudying/src/components/CourseDetail/VideoPlayer/video-player.js
+++ PStudying/src/components/CourseDetail/VideoPlayer/video-player.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native'
-import theme from "../../../globals/theme"
-const VideoPlayer = () => {
-    return (
-        <View style={styles.container}>
-            
-        </View>
-    );
-}
+import React, { useRef, useState, useCallback } from "react";
+import { View, StyleSheet, Button } from "react-native";
+import theme from "../../../globals/theme";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+const VideoPlayer = ({urlLesson}) => {
+  const playerRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+  const [isYoutube, setIsYoutube] = useState(true);
+  
+  return (
+    <View>
+        {isYoutube ? <YoutubePlayer
+      ref={playerRef}
+        height={250}
+        play={playing}
+        videoId={"AHLNzv13c2I"}
+        onError={(error) => console.log(error)}
+        playbackRate={1}
+      /> : <YoutubePlayer
+      ref={playerRef}
+        height={250}
+        play={playing}
+        videoId={"iee2TATGMyI"}
+        onError={(error) => console.log(error)}
+        playbackRate={1}
+      />}
+      
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        height: 200,
-        backgroundColor: theme.RADIUS_BTN_BGR,
-    }
-})
+  container: {
+    height: 200,
+    backgroundColor: theme.RADIUS_BTN_BGR,
+  },
+});
 
 export default VideoPlayer;
--- PStudying/src/components/CourseDetail/course-detail.js
+++ PStudying/src/components/CourseDetail/course-detail.js
@@ -1,20 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import VideoPlayer from "./VideoPlayer/video-player";
-import CourseDetailTabs from "./Course Detail Tabs/course-detail-tab"
+import CourseDetailTabs from "./Course Detail Tabs/course-detail-tab";
 
 const CourseDetail = ({ route, navigation }) => {
   const courseInfo = route.params.courseInfo;
   const [urlLesson, setUrlLesson] = useState(``);
+  const [urlCourse, setUrlCourse] = useState(``);
 
   const onPressLesson = (url) => {
     console.log(url);
     setUrlLesson(url);
-  }
+  };
+  
+  useEffect(() => {
+    console.log(courseInfo.promoVidUrl);
+    setUrlCourse(courseInfo.promoVidUrl);
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
-      <VideoPlayer urlLesson={urlLesson}/>
-      <CourseDetailTabs courseInfo={courseInfo} nav={navigation} onPressLesson={onPressLesson}/>
+      <VideoPlayer urlLesson={urlLesson} urlCourse={urlCourse} />
+      <CourseDetailTabs
+        courseInfo={courseInfo}
+        nav={navigation}
+        onPressLesson={onPressLesson}
+      />
     </View>
   );
 };
