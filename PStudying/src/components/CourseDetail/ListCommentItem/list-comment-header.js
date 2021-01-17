39a0ef08cdc8325486a5a733e959b5147dca4f8b
1712397-KD <1712397@student.hcmus.edu.vn>
2021-01-17 20:39:21 +0700

    upload comment on UI

--- PStudying/src/components/CourseDetail/ListCommentItem/list-comment-header.js
+++ PStudying/src/components/CourseDetail/ListCommentItem/list-comment-header.js
@@ -7,15 +7,28 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import { ratingCourseService } from "../../../core/services/course-services";
 import { AuthenticationContext } from "../../../providers/authentication-provider";
 
-const ListCommentHeader = ({ courseId }) => {
+const ListCommentHeader = ({ courseId, oldList, setNewList }) => {
+  const date = new Date().toUTCString();
   const { state } = useContext(AuthenticationContext);
   const [comment, setComment] = useState(``);
 
-  const postComment = (token, comment, point, courseId) => {
+  const postComment = (token, comment, point, courseId, user) => {
     ratingCourseService(comment, courseId, point, token)
     .then(res=> {
       if (res.status === 200) {
         Alert.alert("Đã thêm bình luận.");
+        setNewList([
+          ...oldList,
+          {
+            id: res.data.id,
+            createdAt: date,
+            content: comment,
+            user : {
+              name: user.name,
+              avatar: user.avatar,
+            }
+          }
+        ])
       } else {
         Alert.alert(res.data.message);
       }
@@ -32,7 +45,7 @@ const ListCommentHeader = ({ courseId }) => {
       inputStyle={{ color: theme.PRIMARY_TEXT_COLOR }}
       rightIcon={
         <TouchableOpacity
-          onPress={() => postComment(state.token, comment, 0, courseId)}
+          onPress={() => postComment(state.token, comment, 0, courseId, state.userInfo)}
         >
           <Icon name="paper-plane" size={24} color={theme.BASIC_BLUE} />
         </TouchableOpacity>
--- PStudying/src/components/CourseDetail/ListComments/list-comments.js
+++ PStudying/src/components/CourseDetail/ListComments/list-comments.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { FlatList } from "react-native";
 import css from "../../../globals/style";
 import { Divider } from "react-native-elements";
@@ -6,16 +6,17 @@ import ListCommentItem from "../ListCommentItem/list-comment-item";
 import CommentBox from "../ListCommentItem/list-comment-header";
 
 const ListComments = ({ fullCourse }) => {
-
+  const [listComment, setListComment] = useState(fullCourse.ratings.ratingList);
+  
   return (
     <FlatList
       style={css.screenContent}
-      data={fullCourse.ratings.ratingList}
+      data={listComment}
       renderItem={({item}) => <ListCommentItem item={item}/>}
       ItemSeparatorComponent={() => <Divider style={css.divider} />}
       keyExtractor={(item) => item.id}
       ListHeaderComponent={
-        <CommentBox courseId={fullCourse.id}/>
+        <CommentBox courseId={fullCourse.id} oldList={listComment} setNewList={setListComment}/>
       }
     />
   );
